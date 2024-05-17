980b55e18e737488be2256b35bf79410415e0811
Georgios Roros <groros2@icloud.com>
2024-05-17 14:03:39 -0400

    added synchronized player movement

--- Authoritative Server/scenes/player_character.gd
+++ Authoritative Server/scenes/player_character.gd
@@ -1,9 +1,13 @@
 extends CharacterBody2D
 
-
+var input: Vector2
+@export var speed: float = 200
 
 func _physics_process(delta):
 	print("process running")
-	velocity = Vector2(20,20)
+	velocity = input * speed
 	move_and_slide()
 
+@rpc("any_peer", "unreliable")
+func get_input(input_vector: Vector2):
+	input = input_vector
